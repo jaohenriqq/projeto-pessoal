6a0060b117514059bbc5674e9433d0a8ab477fe8
João Henrique <joaohen.t8@gmail.com>
2024-11-21 07:31:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*PROGRAMA DECADASTRO DE USUARIOS
+PROJETO PESSOAL
+O usuário pode cadastrar novos usuários.
+O sistema exibe a lista de usuários cadastrados.
+O sistema permite pesquisar um usuário pelo nome.*/
+
 const newaccount= []
 function nomeUsuario(information){
     return(newaccount.unshift(information))
@@ -6,7 +12,7 @@ function nomeUsuario(information){
 
 
 
-do{let pick = prompt(`a quantidade de  usuários cadastrados são:\n ${newaccount.length}\n\n1-cadastrar novo usuário\n2-pesquise pelo nome o usuário cadastrado\n3-SAIR `)
+do{let pick = prompt(`a quantidade de  usuários cadastrados são:\n ${newaccount.length}\n\n1-cadastrar novo usuário\n2-pesquise pelo nome o usuário cadastrado\n3-editar usuário\n4-SAIR `)
 if(pick ==1){
  
  
@@ -17,18 +23,22 @@ if(pick ==1){
  }
  
 
- nomeUsuario(information)
+nomeUsuario(information)
+
 
 }
  
 else if  (pick==2){
 
     let busca = prompt('qual o nome da conta que você procura?')
-    if(busca==information.nome)
-        { alert(`${information.nome}\n${information.email}\n${information.senha}`)
+     let user =  newaccount.find(newaccount=>newaccount.nome==busca)
+if(user){
+  alert  (`${user.nome}\n${user.email}\n${user.senha}`)
+
 }
     
 
 }
 }
-while(pick=3)
+while(pick=4)
+
