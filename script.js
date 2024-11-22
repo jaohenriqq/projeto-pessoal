0dd879bf622590508f8869b15d107b1b6149df38
João Henrique <joaohen.t8@gmail.com>
2024-11-22 11:45:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,34 +8,30 @@ const newaccount= []
 function nomeUsuario(information){
     return(newaccount.unshift(information))
 }
-
 function searchname(busca) {
 return function (user){
-  return newaccount.nome==busca
+  return user.nome==busca
 }
+}function deletname(buscador){
+    return function(conta){
+        return conta.nome && passaword==buscador
+    }
 }
 
-do{let pick = prompt(`a quantidade de  usuários cadastrados são:\n ${newaccount.length}\n\n1-cadastrar novo usuário\n2-pesquise pelo nome o usuário cadastrado\n3-editar usuário\n4-SAIR `)
+do{let pick = prompt(`a quantidade de  usuários cadastrados são:\n ${newaccount.length}\n\n1-cadastrar novo usuário\n2-pesquise pelo nome o usuário cadastrado\n3-mudar senha\n4-fazer login\n5-SAIR `)
 if(pick ==1){
- 
- 
-  let information= {
-   nome:prompt('qual o nome do usuário?'),
+ let information= {
+  nome:prompt('qual o nome do usuário?'),
   email:prompt('qual a email do usuário?'),
   senha:prompt('qual o senha do usuário?')
  }
- 
-
 nomeUsuario(information)
-
-
 }
  
 else if  (pick==2){
-
     let busca = prompt('qual o nome da conta que você procura?')
-     let user = newaccount.find(searchname)
-    alert  (`${user.nome}\n${user.email}\n${user.senha}`)
+    let user = newaccount.find(searchname(busca))
+    alert  (`NOME:${user.nome}\nE-MAIL:${user.email}\nSENHA:${user.senha}`)
 
 
 
@@ -43,14 +39,24 @@ else if  (pick==2){
   
 }
 else if (pick==3){
-  let busca = prompt('?')
-  let user = newaccount.find(searchname)
+  let buscador = prompt('qual o nome da conta do usuário que você está procurando?')
+  let conta = newaccount.find(deletname(buscador))
+  let password=prompt('digite a senha antiga:')
+  let newpassword= prompt('qual sua nova senha')
+
 
 }
-
+else if (pick=4){
+    let login = prompt('qual o email da sua conta?')
+    let senha = prompt('qual a sua senha?')
+    if(login==information.email && senha == information.senha){
+        alert('login realizado!')
+    
+    }
+}
 }
 
-while(pick=4)
+while(pick=5)
 
 
 
